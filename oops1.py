3deed4845166d31cf327815c17cb35521b3fa232
Ritwik Bhardwaj <Ritwik.bhardwaj@metlife.com>
2025-09-06 17:38:59 +0530

    class and obj done

---
+++ oops1.py
@@ -0,0 +1,22 @@
+# initiate a class
+class Employee:
+    # special method/magic method/dunder method - constructer
+    def __init__(self):
+        print("Started executing attributes/data")
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SDE"
+        print("Attributes/data initialized")
+
+        def travel(self, destination):
+            print("This travel function was called manually")
+            print(f"Employee travelling to {destination}")
+
+
+#create an object/instance of a class
+sam = Employee()
+# print(sam.salary)
+# sam.travel("Kerala")
+
+print(type(sam))
+
---
+++ rough.py
@@ -0,0 +1,10 @@
+lst = [1,2,3]
+my_str = "mlops playlist"
+my_int = 155
+
+print(type(lst))
+lst.clear()
+print(lst)
+      
+my_str = my_str.capitalize()
+print(my_str)
